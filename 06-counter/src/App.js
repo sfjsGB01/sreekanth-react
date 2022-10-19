1591003217c82bb0df3707d235028f8536b3a2b4
Sreekanth M. E <sreekanth@revrecap.co>
2022-10-19 20:56:55 +0530

    counter demo

---
+++ 06-counter/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <form>
+        <input type='button' value='Increment' />
+        &nbsp;&nbsp;&nbsp;
+        <input type='button' value='Decrement' />
+      </form>
+
+      <h1>Counter: 0</h1>
+    </div>
+  )
+}
+
+export default App
