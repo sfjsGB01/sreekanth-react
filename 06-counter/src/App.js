cfa75e17b1cce6f9d317ec586b53abb14d3b1b69
Sreekanth M. E <sreekanth@revrecap.co>
2022-10-20 18:22:28 +0530

    completed counter example

--- 06-counter/src/App.js
+++ 06-counter/src/App.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const incrementHandler = () => {
+    console.log('Incrementing...')
+
+    //setCounter(counter + 1)
+    setCounter((prevState) => {
+      return prevState + 1
+    })
+  }
+
+  const decrementHandler = () => {
+    console.log('Decrementing...')
+
+    //setCounter(counter - 1)
+    setCounter((prevState) => {
+      return prevState - 1
+    })
+  }
+
+  console.log('Component rendering: ', counter)
+
   return (
     <div>
       <form>
-        <input type='button' value='Increment' />
+        <input type='button' value='Increment' onClick={incrementHandler} />
         &nbsp;&nbsp;&nbsp;
-        <input type='button' value='Decrement' />
+        <input type='button' value='Decrement' onClick={decrementHandler} />
       </form>
 
-      <h1>Counter: 0</h1>
+      <h1>Counter: {counter}</h1>
     </div>
   )
 }
